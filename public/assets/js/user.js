f9a36a8d55fb5d0999f047203b7b89a68ae4089b
wangzhi <1434533612@qq.com>
2019-10-22 10:21:35 +0800

    wz第二次提交

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,3 +1,4 @@
+// 添加用户部分
 // 当表单发生提交行为的时候，获取表单，为表单设置事件
 $('#userForm').on('submit', function() {
     // 获取用户在提交表单中输入的值，并将其格式化成字符串
@@ -19,8 +20,9 @@ $('#userForm').on('submit', function() {
     return false;
 })
 
+// 上传头像部分
 // 当用户上传头像时，给上传头像按钮添加change事件
-$('#avatar').on('change', function() {
+$('#modifyBox').on('change', '#avatar', function() {
     // new一个formData进行二进制操作
     var formData = new FormData();
     // 在formdata中添加上传了的文件
@@ -40,7 +42,60 @@ $('#avatar').on('change', function() {
             // 实现头像的预览效果
             $('#preview').attr('src', response[0].avatar);
             // 在隐藏域中存储文件的地址
-            $('hiddenAvatar').val(response[0].avatar);
+            $('#hiddenAvatar').val(response[0].avatar);
         }
     });
+})
+
+// 用户列表展示部分
+// 因为页面一打开就要显示用户列表，所以一打开页面直接向服务器端发送请求
+$.ajax({
+    type: "get",
+    url: "/users",
+    success: function(response) {
+        console.log(response);
+        var html = template('userTpl', {
+            data: response,
+        })
+        $('#userBox').html(html)
+    }
+});
+
+// 用户信息修改部分
+// 利用事件委托给userBox下面的编辑按钮添加点击事件
+$('#userBox').on('click', '.edit', function() {
+    // 获取被点击的用户的id  是给编辑按钮设置自定义属性获取id值
+    var id = $(this).attr('data-id');
+    // 根据id值获取用户的详细信息
+    $.ajax({
+        type: "get",
+        url: "/users/" + id,
+        success: function(response) {
+            console.log(response);
+            var html = template('modifyTpl', response);
+            $('#modifyBox').html(html);
+        }
+    });
+})
+
+// 用户修改表单信息提交部分
+// 利用事件委托，添加用户修改表单提交事件
+$('#modifyBox').on('submit', '#modifyForm', function() {
+    // 获取用户修改的表单信息，并且将其转化为字符串类型
+    var formData = $(this).serialize();
+    // 获取这个用户的id值
+    var id = $(this).attr('data-id');
+    // 向服务器端发送请求提交用户修改的表单信息，提交成功后渲染页面
+    $.ajax({
+        // put是修改的提交方式
+        type: "put",
+        url: "/users/" + id,
+        data: formData,
+        success: function(response) {
+            // 当请求发送成功后重新刷新页面
+            location.reload();
+        }
+    });
+    // 阻止页面的默认提交
+    return false;
 })
